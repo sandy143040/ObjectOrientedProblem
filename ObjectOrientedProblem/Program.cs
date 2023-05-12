dfbe35661b52bbc49aabfa4ebad6cda626ccd60d
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-05-12 15:07:33 +0530

    [Sandeep] Modify find max and min element in an array

--- ObjectOrientedProblem/Program.cs
+++ ObjectOrientedProblem/Program.cs
@@ -10,18 +10,35 @@ namespace ObjectOrientedProblem
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the number of elements in the array: ");
-            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter the size of the array: ");
+            int size = int.Parse(Console.ReadLine());
 
-            int[] arr = new int[n];
+            int[] arr = new int[size];
+
             Console.WriteLine("Enter the elements of the array:");
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < size; i++)
             {
                 Console.Write($"Element {i + 1}: ");
                 arr[i] = int.Parse(Console.ReadLine());
             }
 
-            ElementFrequency.CountElementFrequency(arr);
+            int max = arr[0];
+            int min = arr[0];
+
+            for (int i = 1; i < size; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            Console.WriteLine($"Maximum element: {max}");
+            Console.WriteLine($"Minimum element: {min}");
 
             Console.ReadLine();
         }
