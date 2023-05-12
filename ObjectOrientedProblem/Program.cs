2be2988404aaf5b98d2310222028f7f504593410
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-05-12 15:54:21 +0530

    [Sandeep] Modify Reverse each word in a given string

--- ObjectOrientedProblem/Program.cs
+++ ObjectOrientedProblem/Program.cs
@@ -10,35 +10,14 @@ namespace ObjectOrientedProblem
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the size of the array: ");
-            int size = int.Parse(Console.ReadLine());
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
 
-            int[] arr = new int[size];
+            StringOperations stringOps = new StringOperations(str);
 
-            Console.WriteLine("Enter the elements of the array:");
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write($"Element {i + 1}: ");
-                arr[i] = int.Parse(Console.ReadLine());
-            }
+            string reversedString = stringOps.ReverseWords();
 
-            int max = arr[0];
-            int min = arr[0];
-
-            for (int i = 1; i < size; i++)
-            {
-                if (arr[i] > max)
-                {
-                    max = arr[i];
-                }
-                if (arr[i] < min)
-                {
-                    min = arr[i];
-                }
-            }
-
-            Console.WriteLine($"Maximum element: {max}");
-            Console.WriteLine($"Minimum element: {min}");
+            Console.WriteLine($"Reversed string: {reversedString}");
 
             Console.ReadLine();
         }
---
+++ ObjectOrientedProblem/StringOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProblem
+{
+    public class StringOperations
+    {
+        private string str;
+
+        public StringOperations(string str)
+        {
+            this.str = str;
+        }
+
+        public string ReverseWords()
+        {
+            string[] words = str.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] charArray = words[i].ToCharArray();
+                Array.Reverse(charArray);
+                words[i] = new string(charArray);
+            }
+
+            return string.Join(" ", words);
+        }
+    }
+}
